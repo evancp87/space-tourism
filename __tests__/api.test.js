96094c2af118c2fdf80c1fa608efbd2d9453ea69
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-02-28 08:32:54 +0000

    initial jest test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,31 +1,18 @@
-describe('ACTION TESTS', () => {
-  it('should fetch an array of categories', () => {
-    const user = { id: 1, name: 'John', age: 20 }
-    const expectedActions = [
-      {
-        type: fetchUser.pending.type,
-      },
-      {
-        type: action_A.type,
-        payload: {},
-      },
-      {
-        type: fetchUser.fulfilled.type,
-        payload: user,
-      },
-    ]
-    const store = mockStore({})
-    // mock API returns
-    jest
-      .spyOn(importFile, 'getUserById')
-      .mockImplementation(() => Promise.resolve(user))
-    return store.dispatch(fetchUser({ id: 1 })).then(() => {
-      expect(
-        store.getActions().map((action) => ({
-          type: action.type,
-          payload: action.payload,
-        }))
-      ).toEqual(expectedActions)
-    })
-  })
+// import React from 'react'
+// import { render } from '@testing-library/react'
+import PlanetProvider, { SpaceContext } from '../services/Context'
+
+it('should render the current planet name', () => {
+  const TestComponent = () => {
+    const { currPlanet } = React.useContext(SpaceContext)
+    return <div>{currPlanet}</div>
+  }
+
+  const { getByText } = render(
+    <PlanetProvider>
+      <TestComponent />
+    </PlanetProvider>
+  )
+
+  expect(getByText('Moon')).toBeInTheDocument()
 })
