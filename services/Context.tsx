e911341e5cf33ede918657e983d68d53f1775a31
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-03-07 00:13:01 +0000

    added default data functions for pages

--- services/Context.tsx
+++ services/Context.tsx
@@ -1,70 +1,167 @@
-import React, { createContext, Dispatch, useState, useEffect, SetStateAction } from "react";
+import React, {
+  createContext,
+  Dispatch,
+  useState,
+  useEffect,
+  SetStateAction,
+} from 'react'
 
 interface DefaultData {
-  data: Object;
-  setData: Dispatch<SetStateAction<{}>>;
-  planets: Array<string> | undefined;
-  setPlanets: Dispatch<SetStateAction<string[] | undefined>>;
-  currPlanet: string;
-  setCurrPlanet: Dispatch<SetStateAction<string>>;
-  crew: string;
-  setCrew: Dispatch<SetStateAction<string>>;
-  technology: string;
-  setTechnology: Dispatch<SetStateAction<string>>;
+  data: Object
+  setData: Dispatch<SetStateAction<{}>>
+  planets: Array<string> | undefined
+  setPlanets: Dispatch<SetStateAction<string[] | undefined>>
+  currPlanet: string
+  setCurrPlanet: Dispatch<SetStateAction<string>>
+  crew: string
+  setCrew: Dispatch<SetStateAction<string>>
+  technology: string
+  setTechnology: Dispatch<SetStateAction<string>>
+  loadInitialCrew: () => void
+  loadInitialTechnology: () => void
+  loadInitialDestination: () => void
 }
 const SpaceContext = createContext<DefaultData>({
   data: {},
   setData: () => {},
   planets: [],
   setPlanets: () => {},
-  currPlanet: "",
+  currPlanet: '',
   setCurrPlanet: () => {},
-  crew: "",
+  crew: '',
   setCrew: () => {},
-  technology: "",
+  technology: '',
   setTechnology: () => {},
-});
+  loadInitialCrew: () => {},
+  loadInitialTechnology: () => {},
+  loadInitialDestination: () => {},
+})
 
 type ContextProps = {
   children: React.ReactNode
 }
 
-
-// const storage = localStorage.getItem("items")
-//   ? JSON.parse(localStorage.getItem("items"))
-//   : [];
-
 const PlanetProvider = ({ children }: ContextProps) => {
-const [data, setData] = useState({})
-const [planets, setPlanets] = useState<string[] | undefined>([]);
-const [crew, setCrew] = useState("Douglas Hurley")
-const [technology, setTechnology] = useState("Launch Vehicle")
-const [currPlanet, setCurrPlanet] = useState<string>("Moon")
+  const [data, setData] = useState({})
+  const [planets, setPlanets] = useState<string[] | undefined>([])
+  const [crew, setCrew] = useState<string>('Douglas Hurley')
+  const [technology, setTechnology] = useState<string>('Launch Vehicle')
+  const [currPlanet, setCurrPlanet] = useState<string>('Moon')
 
-useEffect(() => {
-  fetch( "../data.json")
-    .then((response) => response.json())
-    .then((data) => {
-      setData(data);
-    });
-}, []);
+  // .then((response) => response.json())
+  // .then((planets) =>
+  //   setPlanets(
+  //     planets.filter((planet: any) => {
+  //       const {
+  //         name,
+  //         overview: { content: overviewContent, source: overviewSource },
+  //         structure: { content: structureContent, source: structureSource },
+  //         geology: { content: geometryContent, source: geometrySource },
+  //         rotation,
+  //         revolution,
+  //         radius,
+  //         temperature,
+  //         images: {
+  //           planet: planetImage,
+  //           internal: planetInternalImage,
+  //           geology: planetGeologyInternal,
+  //         },
+  //       } = planet;
+  //       if (name === currPlanet) {
+  //         setData({
+  //           name,
+  //           overviewContent,
+  //           overviewSource,
+  //           structureContent,
+  //           structureSource,
+  //           geometryContent,
+  //           geometrySource,
+  //           rotation,
+  //           revolution,
+  //           radius,
+  //           temperature,
+  //           planetImage,
+  //           planetInternalImage,
+  //           planetGeologyInternal,
+  //         });
+  //       }
+  //     })
+  //   )
+  // );
+  //       }
+  //     })
+  //     );
+  //   })
+
+  async function loadInitialTechnology(currPlanet) {
+    const response = await fetch('../../data.json')
+    const data = await response.json()
+
+    return data.technology
+      .filter((tech) => tech.name === currPlanet)
+      .map((tech) => ({
+        name: tech.name,
+        pngImage: tech.images.png,
+        webpImage: tech.images.webp,
+        description: tech.description,
+        distance: tech.distance,
+        travel: tech.travel,
+      }))
+  }
+
+  async function loadInitialDestination(currPlanet) {
+    const response = await fetch('../../data.json')
+    const data = await response.json()
+
+    return data.destinations
+      .filter((destination) => destination.name === currPlanet)
+      .map((destination) => ({
+        name: destination.name,
+        pngImage: destination.images.png,
+        webpImage: destination.images.webp,
+        description: destination.description,
+        distance: destination.distance,
+        travel: destination.travel,
+      }))
+  }
+
+  async function loadInitialCrew(currPlanet) {
+    const response = await fetch('../../data.json')
+    const data = await response.json()
+
+    return data.crew
+      .filter((c) => c.name === currPlanet)
+      .map((c) => ({
+        name: c.destination.name,
+        pngImage: c.images.png,
+        webpImage: c.images.webp,
+        description: c.description,
+        distance: c.distance,
+        travel: c.travel,
+      }))
+  }
 
   return (
-    <SpaceContext.Provider value={{
-      data, 
-      setData, 
-      planets, 
-      setPlanets, 
-      crew, 
-      setCrew, 
-      technology, 
-      setTechnology, 
-      currPlanet, 
-      setCurrPlanet
-  }}>
+    <SpaceContext.Provider
+      value={{
+        data,
+        setData,
+        planets,
+        setPlanets,
+        crew,
+        setCrew,
+        technology,
+        setTechnology,
+        currPlanet,
+        setCurrPlanet,
+        loadInitialCrew,
+        loadInitialTechnology,
+        loadInitialDestination,
+      }}
+    >
       {children}
-  </SpaceContext.Provider>
-  );
-};
+    </SpaceContext.Provider>
+  )
+}
 
-export default PlanetProvider;
+export default PlanetProvider
